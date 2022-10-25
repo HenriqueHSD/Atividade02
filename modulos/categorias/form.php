95ce36ea74430b5724100078b156367b70faacfd
HenriqueHSD <62896890+HenriqueHSD@users.noreply.github.com>
2022-10-24 23:01:38 -0300

    Add files via upload

---
+++ modulos/categorias/form.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form method="POST" action="resultado.php">
+        Celular: <input type="text" name="calular"/> <br/>
+        Roupa: <input type="text" name="roupa"/> <br/>
+        Sapato: <input type="text" name="sapato"/> <br/>
+        Livro: <input type="text" name="livro"/> <br/>
+        Processador: <input type="text" name="processador"/> <br/>
+        <input type="submit" name="button" value="Enviar"/>
+
+        </form>
+
+    </body>
+</html>
